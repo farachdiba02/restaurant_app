2460cd1b8fad8eb3390e0b6e7fa6d3af1af43206
farachdiba02 <farachdiba@icloud.com>
2023-11-07 22:13:07 +0700

    feat: cms done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./*.{html, js}"],
   theme: {
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
     extend: {},
   },
   plugins: [],
